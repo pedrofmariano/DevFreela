58c9339513a19943acd7fd7ee77c0660b7a1ab8c
Pedro Henrique Fortunato Silva Mariano <pedroh.fortunatos@gmail.com>
2021-08-21 19:20:16 -0300

    atualização de projeto

--- DevFreela.Application/ViewModels/ProjectViewModel.cs
+++ DevFreela.Application/ViewModels/ProjectViewModel.cs
@@ -8,12 +8,14 @@ namespace DevFreela.Application.ViewModels
 {
     public class ProjectViewModel
     {
-        public ProjectViewModel(string title, DateTime creaatedAt)
+        public ProjectViewModel(int id, string title, DateTime creaatedAt)
         {
+            Id = id;
             Title = title;
             CreaatedAt = creaatedAt;
         }
 
+        public int Id { get; set; }
         public string Title { get; private set; }
         public DateTime CreaatedAt { get; private set; }
     }
---
+++ DevFreela/Controllers/ProjectController.cs
@@ -0,0 +1,61 @@
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService; 
+        }
+
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            var projects = _projectService.GetAll(query);
+
+            return Ok(projects);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var project = _projectService.GetById(id);
+
+            if(project == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(project);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] NewProjectInputModel inputModel)
+        {
+            if (inputModel.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            var id = _projectService.Create(inputModel);
+
+            return CreatedAtAction(nameof(GetById), new { id = id }, inputModel);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectInputModel inputModel)
+        {
+            if(inputModel.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+            _projectService.Update(inputModel);
+
+            return NoContent();
+        }
+    }  
+}
